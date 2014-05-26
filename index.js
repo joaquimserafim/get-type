55a94a1b797df4b179a452315108c780bbef982d
joaquimserafim <joaquim.serafim@gmail.com>
2014-05-27 00:52:55 +0100

    tests for type.get are ready

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ module.exports = new Type();
 
 
 function Type () {
-  if (!(this instanceof Type)) return new Type();
-
   // put isJSON in first
   this._is = ['isJSON'];
   this.isJSON = isJSON;
 
   // lets make use of core-util-is
   Object.keys(is).forEach(function (fn) {
+    // lets jump isobject & isnullorundefined
+    if (['isObject', 'isNullOrUndefined'].indexOf(fn) !== -1) return;
     this[fn] = is[fn];
     this._is.push(fn);
   }, this);
+
+  // isObject in last because to get Regex, Date & Error
+  this._is.push('isObject', 'isNullOrUndefined');
+  this.isObject = is.isObject;
+  // NullOrUndefined is only to be used directly
+  // with type.isNullOrUndefined
+  this.isNullOrUndefined = is.isNullOrUndefined;
 }
 
 Type.prototype.get = function (op) {
@@ -24,8 +31,4 @@ Type.prototype.get = function (op) {
     if (this[this._is[i]](op))
       return this._is[i].replace('is', '').toLowerCase();
   }
-};
-
-Type.prototype.convert = function (value) {
-  return value;
 };
--- test.js
+++ test.js
@@ -1,27 +1,20 @@
-
-
-
+var test = require('tape');
 var type = require('./');
 
+test('getting types', function (t) {
+  t.plan(13);
 
-console.log(type);
-console.log();
-
-console.log(type.get('121212.1212'));
-console.log(type.get('test'));
-console.log(type.get(1212121212));
-console.log(type.get(null));
-console.log(type.get());
-console.log(type.get('{"a": 1212, "b": "Hello World"}'));
-
-console.log();
-
-console.log(type.convert('121212.1212'));
-console.log(type.convert('test'));
-console.log(type.convert(1212121212));
-console.log(type.convert(null));
-console.log(type.convert());
-console.log(type.convert('{"a": 1212, "b": "Hello World"}'));
-
-
-
+  t.deepEqual(type.get('{"a": 2014, "b": [1,2]}'), 'json', 'should return a json');
+  t.deepEqual(type.get([1,2,3,4]), 'array', 'should return an array');
+  t.deepEqual(type.get(true), 'boolean', 'should return a boolean');
+  t.deepEqual(type.get(null), 'null', 'should return null');
+  t.deepEqual(type.get(2014), 'number', 'should return a  number');
+  t.deepEqual(type.get('a string message'), 'string', 'should return a string');
+  t.deepEqual(type.get(), 'undefined', 'should return undefined');
+  t.deepEqual(type.get(/w/), 'regexp', 'should return a regexp');
+  t.deepEqual(type.get({a:1}), 'object', 'should return an object');
+  t.deepEqual(type.get(new Date()), 'date', 'should return a date object');
+  t.deepEqual(type.get(new Error()), 'error', 'should return an error object');
+  t.deepEqual(type.get(function () {}), 'function', 'should return a function');
+  t.deepEqual(type.get(new Buffer('')), 'buffer', 'should return a buffer');
+});
